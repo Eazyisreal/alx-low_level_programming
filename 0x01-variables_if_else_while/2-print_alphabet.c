1a7b75d22c4dd8b9c3d38ad0027a54b90755dbee
Eazyisreal <Eazyisreal192@gmail.com>
2022-09-12 06:07:39 -0700

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,39 @@
+lude <stdio.h>
+
+
+
+/**
+ *
+ *  * main - Entry point
+ *
+ *   *
+ *
+ *    * Return: Always 0 (Success)
+ *
+ *     */
+
+int main(void)
+
+{
+
+		char c = 'a';
+
+
+
+			while (c <= 'z')
+
+					{
+
+								putchar(c);
+
+										c++;
+
+											}
+
+				putchar('\n');
+
+
+
+					return (0);
+
+}
