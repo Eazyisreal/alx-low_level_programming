568f52b4da91837f4b7e7da46063e52883a86f9b
Eazyisreal <eazyisreal192@gmail.com>
2022-09-15 12:39:49 +0100

    main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,18 +1,41 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef _MAIN_
+#define _MAIN_
 
-int _putchar(char c);
-void print_alphabet(void);
-void print_alphabet_x10(void);
-int _islower(int c);
-int _isalpha(int c);
-int print_sign(int n);
-int _abs(int);
-int print_last_digit(int);
-void jack_bauer(void);
-void times_table(void);
-int add(int, int);
-void print_to_98(int n);
-void print_times_table(int n);
+#include <stdio.h>
 
-#endif
+/**
+ *  * positive_or_negative - checks if an integer is positive,negative or zero
+ *   * @i: integer argument
+ *    */
+
+void positive_or_negative(int i);
+
+/**
+ *  * largest_number - returns the largest of three numbers
+ *   * @a: first integer
+ *    * @b: second integer
+ *     * @c: third integer
+ *      * Return: largest number
+ *       */
+
+int largest_number(int a, int b, int c);
+
+/**
+ *  * convert_day - convert the months and year to days
+ *   * @month: first argument
+ *    * @day: second argument
+ *     * Return: integer
+ *      */
+
+int convert_day(int month, int day);
+
+/**
+ *  * print_remaining_days - function to print remaining days of the year
+ *   * @month: first argument
+ *    * @day: second argument
+ *     * @year: current year
+ *      * Return: void
+ *       */
+
+void print_remaining_days(int month, int day, int year);
+
