d1c013f990644b5119b14413430ade8e8d7acd06
Eazyisreal <eazyisreal192@gmail.com>
2023-03-18 16:48:11 +0100

    Task O - Allocate memory using malloc

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,6 +1,18 @@
-void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
-int *array_range(int min, int max);
+lines (9 sloc)  325 Bytes
+
+#ifndef _MAIN_
+#define _MAIN_
+
 void *malloc_checked(unsigned int b);
+
 char *string_nconcat(char *s1, char *s2, unsigned int n);
+
 void *_calloc(unsigned int nmemb, unsigned int size);
+
+int *array_range(int min, int max);
+
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
+
 int _putchar(char c);
+
+#endif
