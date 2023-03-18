195ea1add0f04b8db971dee54487e72016b04b50
Eazyisreal <eazyisreal192@gmail.com>
2023-03-18 16:57:02 +0100

    Task O - Allocate memory using malloc

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -10,13 +10,13 @@
 
 void *malloc_checked(unsigned int b)
 {
-		void *p;
+	void *p;
 
-		p = malloc(b);
+	p = malloc(b);
 
-		if (p == NULL)
-			exit(98);
-			
-		return (p);
+	if (p == NULL)
+		exit(98);
+		
+	return (p);
 }
 
