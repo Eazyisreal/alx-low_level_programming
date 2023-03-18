3a26023c672e8b5bf0b304ad3c230935b34decc1
Eazyisreal <eazyisreal192@gmail.com>
2023-03-18 17:13:24 +0100

    Task 3 - Create Array of Integers

---
+++ 0x0C-more_malloc_free/33-array_range.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * array_range - array of integers
+ * @min: start range
+ * @max: end range
+ * Return: pointer to array
+ */
+
+int *array_range(int min, int max)
+{
+	int *ptr;
+	int i, k;
+	int j = (max - min + 1);
+
+	if (min > max)
+		return (NULL);
+
+	ptr = malloc(j * sizeof(int));
+	if (ptr == NULL)
+		return (NULL);
+
+	for (i = 0, k = min; i < j; i++, k++)
+		*(ptr + i) = k;
+
+	return (ptr);
+}
