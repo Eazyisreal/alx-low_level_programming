1060eb4b0b65fbd522461d32e88a16288eac63e2
eazyisreal <eazyisreal192@gmail.com>
2023-05-19 15:07:49 -0700

    Task done

--- 0x17-doubly_linked_lists/7-insert_dnodeint.c
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -54,4 +54,4 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
 	if (idx == 1)
 		return (add_dnodeint_end(h, n));
 	return (NULL);
-}   
+}
---
+++ 0x17-doubly_linked_lists/script.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+directory="/alx-low_level_programming/0x17-doubly_linked_lists"  # Replace with your target directory
+command_to_run="betty"   # Replace with your desired command
+
+for file in "$directory"/*; do
+    if [ -f "$file" ]; then
+        "$command_to_run" "$file"
+    fi
+done
+
+
